949a9a8b2374e1d54e945742f059057a34058470
Michael <mida.santiago98@gmail.com>
2025-04-01 05:25:30 -0400

    Initialized Homepage Layout with Styling and data

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="footer p-4 mt-15">
+            <h4 className="footer-email">Email: mida.santiago98@gmail.com</h4>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/projectData.js
@@ -0,0 +1,22 @@
+const projects = [
+    {
+        id: "elden-ring-build-manager",
+        title: "Elden Ring Build Manager",
+        description: "Track and compare character stats and equipment.",
+        image: "src/assets/er-build-manager.png",
+        technologies: ["React", "Node.js", "MongoDB", "Tailwind"],
+        link: "/projects/elden-ring-build-manager",
+        githubLink: "https://github.com/Midasantiago/ER-Stat-Checker",
+        deployedLink: "https://er-stat-checker.onrender.com",
+    },
+    {
+        id: "zombies-guide",
+        title: "Call of Duty Zombies Guide",
+        description: "Interactive guide for map details and completing Easter Eggs.",
+        image: "",
+        technologies: ["React", "Redux", "Firebase"],
+        link: "/projects/zombies-guide",
+    },
+];
+
+export default projects;
