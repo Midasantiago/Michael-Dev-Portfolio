2640a7507bd551f39d27b05d9bfc6787619c212a
Michael <mida.santiago98@gmail.com>
2025-04-09 20:11:27 -0400

    Implemented Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect} from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import ReactGA from "react-ga4";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
 
   const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize('G-ZXKGVMW4P3');
+    ReactGA.send("pageview");
+  }, []);
 
   return (
     <div className="relative">
